b0149f84c0639e49f60110d517049cad92162a76
Nick <nick.gilliam2@gmail.com>
2021-05-13 18:01:48 -0400

    added user dropdown to nav

--- core/static/js/base_auth.js
+++ core/static/js/base_auth.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
     $('.profile-picture-icon').each(function(){
         $(this).width($(this).height());
     });
+
+    $(document).on('click', ':not(#user-dropdown)', function(e){
+        if ($(this).prop('id') == 'user-dropdown' || $(this).parents('#user-dropdown').length > 0){
+            e.stopPropagation();
+
+            return;
+        }
+
+        if ($(this).prop('id') == 'user-container' || $(this).parents('#user-container').length > 0){
+            if ($('#user-dropdown').css('display') == 'flex')
+                $('#user-dropdown').css('display', 'none');
+            else
+                $('#user-dropdown').css('display', 'flex');
+            
+            e.stopPropagation();
+        } else
+            $('#user-dropdown').css('display', 'none');
+    });
 });
